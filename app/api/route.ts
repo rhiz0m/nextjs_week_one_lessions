229717702b0f1dea2f74a5950c8659bed2e183a6
taokyone <antonsson.andreas@hotmail.com>
2023-11-21 11:51:57 +0100

    GET Request and creating an JSON Object

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,1 +1,17 @@
+import { NextResponse } from "next/server";
 
+export async function GET(request: Request) {
+  try {
+    // Your GET request handling logic here
+    return NextResponse.json({
+      message: "GET request success - Hello Next.JS World",
+      myId: 2023,
+      school: "STI, Stockholms Tekniska Institut",
+      course: "Web Communication, API and Backend",
+      framework: "Next.js",
+      programLanguages: "Typescript/Javascript",
+    });
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <p className="text-black">Hello World</p>
+        <p className="text-black">Hello Next World</p>
 
         <p>Summan: {4 + 4}</p>
         <p>{4 > 2 ? "TRUE" : "FALSE"}</p>
       </div>
     </main>
-  )
+  );
 }
