95a936d17da6b077832f1f1c03fe781ca89a984d
taokyone <antonsson.andreas@hotmail.com>
2023-11-23 16:58:47 +0100

    uppgifter. Lektion 2

--- app/lession2/page.tsx
+++ app/lession2/page.tsx
@@ -1,10 +1,66 @@
 import Image from "next/image";
+import { Interface } from "readline";
+
+interface Book {
+  name: string;
+  author: string;
+  amountOfPages: number;
+  isPublished: boolean;
+}
+
+interface Student {
+  name: string;
+  age: number;
+  books: Book[];
+}
+
+interface Animal {
+  species: string;
+  canFly: boolean;
+}
+
+const whiteShark: Animal = {
+  species: "fish",
+  canFly: false,
+};
+
+const eagle: Animal = {
+  species: "birds",
+  canFly: true,
+};
+
+const ArrayOfAnimals: Animal[] = [whiteShark, eagle];
+
+const name: string = "";
+const num: number = 0;
+const bool: boolean = false;
+const numArray: number[] = [];
+
+enum dayOfTheWeek {
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+  sunday,
+}
+
+let pickAday: dayOfTheWeek = dayOfTheWeek.monday;
 
 export default function lektion2() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <p className="text-black bold font-bold">Lektion 2</p>
+        <p>First day of the week: {pickAday} (Index)</p>
+        <ul>
+          {ArrayOfAnimals.map((animal, index) => (
+            <li key={index}>
+              Species: {animal.species}, Can fly: {animal.canFly.toString()}
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
