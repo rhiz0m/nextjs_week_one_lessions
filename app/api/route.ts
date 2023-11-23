febdf985579a2b38cc616d28328fde3b190db20c
taokyone <antonsson.andreas@hotmail.com>
2023-11-23 19:40:49 +0100

    Lession 1 & 2 tasks complete.

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,14 +4,16 @@ export async function GET(request: Request) {
   try {
     // Your GET request handling logic here
     return NextResponse.json({
-      message: "GET request success - Hello Next.JS World",
+      message: "GET request success - Hejsan svejsan Next.JS World",
       myId: 2023,
       school: "STI, Stockholms Tekniska Institut",
       course: "Web Communication, API and Backend",
       framework: "Next.js",
       programLanguages: "Typescript/Javascript",
-    });
+     },
+    { status: 200}
+    );
   } catch (error) {
-    return NextResponse.error();
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
 }
--- app/lession1/page.tsx
+++ app/lession1/page.tsx
@@ -5,8 +5,13 @@ export default function lektion1() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <p className="text-black bold font-bold">Lession 1</p>
-        <p>Summan: {4 + 4}</p>
+        <p>Hello Nest.js World</p>
+        <p>Summan: {2 + 2}</p>
         <p>{4 > 2 ? "TRUE" : "FALSE"}</p>
+        <p>{2 < 4 ? "<p></p>" : "<h1> </h1>"}</p>
+        <p>{2 + 2 == 99 || 2 + 2 == 4 ? "TRUE" : "FALSE"}</p>
+        <p>{2 + 2 == 4 && 2 + 4 == 6 ? "TRUE" : "FALSE"}</p>
+        <p></p>
       </div>
     </main>
   );
